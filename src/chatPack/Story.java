57eb1951b47f5198a35fbfb051ba6eebf44d019d
moyehya <87992100+acmacid@users.noreply.github.com>
2022-04-18 11:39:37 +0200

    Update Story.java

--- src/chatPack/Story.java
+++ src/chatPack/Story.java
@@ -1,4 +1,26 @@
 package chatPack;
 //Task for : Mohamed yehia
 public class Story {
+    private  String storyText;
+    private String  time;
+    public Story(String text,String date,String time)
+    {
+        this.storyText=text;
+        this.time=time;
+    }
+    public void setStoryText(String text) {
+        this.storyText=text;
+    }
+    public void SetTime(String time) {
+        this.time=time;
+    }
+    public String getStoryText()
+    {
+        return storyText;
+    }
+    public String getTime()
+    {
+        return time;
+    }
+
 }
