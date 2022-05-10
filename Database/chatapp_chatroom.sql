bb2747d1d84c3798ba5c0fce9fabed973b675868
MohamedWalidxX <103504375+MohamedWalidxX@users.noreply.github.com>
2022-05-10 11:26:40 +0200

    LastLastDb

--- Database/chatapp_chatroom.sql
+++ Database/chatapp_chatroom.sql
@@ -26,6 +26,7 @@ CREATE TABLE `chatroom` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `dateOfGroupCreation` date DEFAULT (curdate()),
+  `timeOfGroupCreation` time DEFAULT (curtime()),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -36,7 +37,7 @@ CREATE TABLE `chatroom` (
 
 LOCK TABLES `chatroom` WRITE;
 /*!40000 ALTER TABLE `chatroom` DISABLE KEYS */;
-INSERT INTO `chatroom` VALUES (1,'private','2022-05-10'),(2,'private','2022-05-10'),(3,'private','2022-05-10'),(4,'TestingGroupFirst','2022-05-10');
+INSERT INTO `chatroom` VALUES (1,'private','2022-05-10','11:18:33'),(2,'private','2022-05-10','11:18:33'),(3,'private','2022-05-10','11:18:33'),(4,'TestingGroupFirst','2022-05-10','11:18:33');
 /*!40000 ALTER TABLE `chatroom` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-10 11:11:08
+-- Dump completed on 2022-05-10 11:25:42
